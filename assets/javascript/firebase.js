63ddbb83e9cf64d7eab7386b42c6cf92ee29c7df
alexandra0213 <aexl.dx021@gmail.com>
2024-10-18 15:25:57 -0500

    actualizando

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,8 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+
+// Autenticación
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+
+// Firestore
+import {
+  getFirestore,
+  collection,
+  doc,
+  addDoc,
+  onSnapshot,
+  deleteDoc,
+  updateDoc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAl8sxovu7G-Lou6yDeNt1Yxq4iwar4V6I",
@@ -14,4 +29,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+// Inicializar Autenticacion de Firebase y obtener una referencia del servicio
+export const auth = getAuth(app);
+// Inicializar Firestore
+export const db = getFirestore();
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,25 +1,45 @@
+// Importamos la app
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+
+//funcionalidad de ancors
+
 $(document).ready(function() {
-  $('#showRegisterLink').click(function(e) {
-      e.preventDefault();
-      $('#loginSection').addClass('d-none');
-      $('#registroSection').removeClass('d-none');
-  });
+    $('#showRegisterLink').click(function(e) {
+        e.preventDefault();
+        $('#loginSection').addClass('d-none');
+        $('#registroSection').removeClass('d-none');
+    });
 
-  $('#showLoginLink').click(function(e) {
-      e.preventDefault();
-      $('#registroSection').addClass('d-none');
-      $('#loginSection').removeClass('d-none');
-  });
+    $('#showLoginLink').click(function(e) {
+        e.preventDefault();
+        $('#registroSection').addClass('d-none');
+        $('#loginSection').removeClass('d-none');
+    });
 
-  $('#forgotPasswordLink').click(function(e) {
-      e.preventDefault();
-      $('#loginSection').addClass('d-none');
-      $('#recuperarContainer').removeClass('d-none');
-  });
+    $('#forgotPasswordLink').click(function(e) {
+        e.preventDefault();
+        $('#loginSection').addClass('d-none');
+        $('#recuperarContainer').removeClass('d-none');
+    });
 
-  $('#volverBtn').click(function(e) {
-      e.preventDefault();
-      $('#recuperarContainer').addClass('d-none');
-      $('#loginSection').removeClass('d-none');
-  });
+    $('#volverBtn').click(function(e) {
+        e.preventDefault();
+        $('#recuperarContainer').addClass('d-none');
+        $('#loginSection').removeClass('d-none');
+    });
+
+    $('#showLoginWith').click(function(e) { // Cambié el ID aquí
+        e.preventDefault();
+        $('#loginSection').addClass('d-none');
+        $('#loginwith').removeClass('d-none'); // Asegúrate de que esto sea el ID correcto
+    });
+
+    $('#backToLogin').click(function(e) {
+        e.preventDefault();
+        $('#loginwith').addClass('d-none');
+        $('#loginSection').removeClass('d-none');
+    });
 });
+
+
